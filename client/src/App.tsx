fe4c1d9fe1038d9346d97255057f068966f68c0a
Misho1019 <mikailtorres99@gmail.com>
2026-07-08 14:24:12 +0300

    init: client scaffold

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#0D0A08] flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-[#D4A04A]">Roast & Ritual</h1>
+    </div>
+  )
+}
+
+export default App
